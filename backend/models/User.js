c1768034dfc8e9d4218db15b0d939eb353b735f8
akwanmaroso <akwanmaroso@gmail.com>
2020-02-14 08:35:31 +0800

    update userController and add model User.js

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  nama: {
+    type: String,
+    required: true
+  },
+  money: {
+    type: Number,
+    required: true
+  },
+})
