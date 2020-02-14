31364b1584e8dcf32ef48209e2cb4fa97f2a51fd
akwanmaroso <akwanmaroso@gmail.com>
2020-02-14 09:52:52 +0800

    add createDataUser and give validation

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const validatejs = require('validatorjs');
 
 exports.index = (req, res) => {
   res.send('halo');
@@ -9,17 +10,44 @@ exports.login = (req, res) => {
 }
 
 exports.createdata = (req, res) => {
-  let data = {req};
+  const data = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    nama: req.body.nama,
+  };
+  const rules = {
+    username: 'required|min:3',
+    email: 'required',
+    password: 'required|min:4',
+    nama: 'required|min:4',
+  };
 
-  // let rules = {
-  //   username: 'required|min:3',
-  //   email: 'required',
-  //   password: 'required|min:4',
-  //   nama: 'required|min:4',
-  //   money: 'required',
-  // };
-  console.log(data);
-  return data;
+  const validate = new validatejs(data, rules);
+  if (validate.fails()) {
+    res.status(422).send({
+      errors: {
+        username: validate.errors.first('username'),
+        email: validate.errors.first('email'),
+        password: validate.errors.first('password'),
+        nama: validate.errors.first('nama')
+      }
+    })
+  }
+
+  const user = new User(data);
+
+  try {
+    user.save();
+    console.log(user);
+    res.status(201).send({
+      message: 'user berhasil ditambahkan'
+    })
+  } catch (error) {
+    console.log(`Nessage = ${error} ` );
+    
+  }
+  // return data;
 
 }
 
