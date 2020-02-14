01a019d4d6783691a084c60be1d99ae11377c49e
akwanmaroso <akwanmaroso@gmail.com>
2020-02-14 08:36:40 +0800

    update userController and add model User.js

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,7 +1,15 @@
 exports.index = (req, res) => {
-  res.send('halo')
+  res.send('halo');
 }
 
 exports.login = (req, res) => {
   
+}
+
+exports.addData = (req, res) => {
+
+}
+
+exports.showData = (req, res) => {
+
 }
