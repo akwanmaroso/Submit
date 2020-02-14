1b86b54b5e08eb09f4ce3d04686131752461d9d1
akwanmaroso <akwanmaroso@gmail.com>
2020-02-14 09:07:38 +0800

    add route post to createData

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -4,5 +4,6 @@ const UserController = require('../controllers/UserController');
 
 routes.get('/', UserController.index);
 routes.get('/login', UserController.index);
+routes.post('/createdata', UserController.createdata);
 
 module.exports = routes;
