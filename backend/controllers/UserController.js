c90bcf8b6136aeef83c0c9f1aefebe279ebd8e41
akwanmaroso <akwanmaroso@gmail.com>
2020-02-14 09:08:27 +0800

    add more function

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 exports.index = (req, res) => {
   res.send('halo');
 }
@@ -6,7 +8,18 @@ exports.login = (req, res) => {
   
 }
 
-exports.addData = (req, res) => {
+exports.createdata = (req, res) => {
+  let data = {req};
+
+  // let rules = {
+  //   username: 'required|min:3',
+  //   email: 'required',
+  //   password: 'required|min:4',
+  //   nama: 'required|min:4',
+  //   money: 'required',
+  // };
+  console.log(data);
+  return data;
 
 }
 
