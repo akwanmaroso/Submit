2ea8caec270a4c1b845238bdaff7e35283fd19e2
akwanmaroso <akwanmaroso@gmail.com>
2020-02-18 20:57:35 +0800

    fix login page

--- mobileApp/src/login/Login.js
+++ mobileApp/src/login/Login.js
@@ -7,6 +7,7 @@ import {
   Text,
   StatusBar,
   TextInput,
+  Image,
   TouchableOpacity
 } from 'react-native';
 
@@ -14,14 +15,17 @@ export default class Login extends Component {
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: '#eee', justifyContent: 'center'}}>
+        <View style={{alignItems: 'center', justifyContent: 'space-between', height: 90}}>
+          <Image source={{ uri: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png'}} style={{width: 90, height: 90}}/>
+        </View>
         <View style={{marginHorizontal: 16}}>
           <View>
-            <Text style={{fontSize: 24, fontWeight: 'bold', color: '#1C1C1C',}}>Email</Text>
-            <TextInput style={{height: 40, width: '100%', borderColor: 'black', borderWidth: 1}}/>
+            <Text style={{fontSize: 24, fontWeight: 'bold', color: '#1C1C1C'}}>Email</Text>
+            <TextInput style={{height: 40, width: '100%', borderColor: 'black', borderWidth: 1, paddingLeft: 10}}/>
           </View>
           <View>
-            <Text style={{fontSize: 24, fontWeight: 'bold', color: '#1C1C1C',}}>Password</Text>
-            <TextInput style={{height: 40, width: '100%', borderColor: 'black', borderWidth: 1}}/>
+            <Text style={{fontSize: 24, fontWeight: 'bold', color: '#1C1C1C'}}>Password</Text>
+            <TextInput style={{height: 40, width: '100%', borderColor: 'black', borderWidth: 1, paddingLeft: 10, }}/>
           </View>
           <View style={{alignSelf: 'flex-end', marginTop: 14}}>
             <TouchableOpacity style={{ backgroundColor: '#eb3434', padding: 16}}>
@@ -33,3 +37,12 @@ export default class Login extends Component {
     )
   }
 }
+
+// const styles = new StyleSheet({
+//   label: {
+//     fontSize: 24, 
+//     fontWeight: 'bold', 
+//     color: '#1C1C1C'
+//   },
+
+// })
