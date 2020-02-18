a14977bb5d732e11155206ec6fb9c4e322bb9a10
akwanmaroso <akwanmaroso@gmail.com>
2020-02-18 22:37:26 +0800

    update login v2

--- mobileApp/src/login/Login.js
+++ mobileApp/src/login/Login.js
@@ -14,22 +14,22 @@ import {
 export default class Login extends Component {
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: '#eee', justifyContent: 'center'}}>
-        <View style={{alignItems: 'center', justifyContent: 'space-between', height: 90}}>
-          <Image source={{ uri: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png'}} style={{width: 90, height: 90}}/>
+      <View style={{ flex: 1, backgroundColor: '#00E676', justifyContent: 'center'}}>
+        <View style={{alignItems: 'center'}}>
+          <Text style={{fontSize: 30, fontWeight: 'bold'}}>SAKU-KU</Text>  
         </View>
         <View style={{marginHorizontal: 16}}>
           <View>
-            <Text style={{fontSize: 24, fontWeight: 'bold', color: '#1C1C1C'}}>Email</Text>
-            <TextInput style={{height: 40, width: '100%', borderColor: 'black', borderWidth: 1, paddingLeft: 10}}/>
+            <Text style={styles.label}>Email</Text>
+            <TextInput style={styles.input} />
           </View>
-          <View>
-            <Text style={{fontSize: 24, fontWeight: 'bold', color: '#1C1C1C'}}>Password</Text>
-            <TextInput style={{height: 40, width: '100%', borderColor: 'black', borderWidth: 1, paddingLeft: 10, }}/>
+          <View style={{marginTop: 15}}>
+            <Text style={styles.label}>Password</Text>
+            <TextInput style={styles.input} />
           </View>
-          <View style={{alignSelf: 'flex-end', marginTop: 14}}>
-            <TouchableOpacity style={{ backgroundColor: '#eb3434', padding: 16}}>
-                <Text style={{fontSize: 15, color: 'white'}}>Login</Text>
+          <View style={{alignSelf: 'center', marginTop: 20}}>
+            <TouchableOpacity style={{ backgroundColor: '#eb3434', padding: 16, borderRadius: 25,  }}>
+                <Text style={{fontSize: 16, color: 'white', textAlign: 'center', paddingHorizontal: 46}}>Login</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -38,11 +38,19 @@ export default class Login extends Component {
   }
 }
 
-// const styles = new StyleSheet({
-//   label: {
-//     fontSize: 24, 
-//     fontWeight: 'bold', 
-//     color: '#1C1C1C'
-//   },
-
-// })
+const styles = StyleSheet.create({
+  label: {
+    fontSize: 17, 
+    fontWeight: 'bold', 
+    color: '#1C1C1C'
+  },
+  input: {
+    height: 54, 
+    width: '100%', 
+    paddingLeft: 10, 
+    backgroundColor: 'white', 
+    marginTop:4, 
+    borderRadius: 10, 
+    fontSize: 15,
+  }
+})
