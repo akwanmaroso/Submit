25244ee0b9e3b91eeaab462356af52f9a49189ba
akwanmaroso <akwanmaroso@gmail.com>
2020-02-21 10:08:51 +0800

    fetching api

--- mobileApp/App.js
+++ mobileApp/App.js
@@ -5,22 +5,34 @@ import {
   ScrollView,
   View,
   Text,
+  FlatList,
   StatusBar,
   Image
 } from 'react-native';
 import Login from './src/pages/Login'
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs'
 import NavbarButton from './src/NavbarBtn';
+import Axios from 'axios';
 
 class App extends Component {
+  state = {
+    user: {}
+  }
+
+  async componentDidMount () {
+    const user = await Axios.get('http://10.0.2.2:4900/api/5e45fd1272c2321d88ef4c6d/showdata');
+    this.setState({
+      user: user.data.data
+    })
+    console.log(this.state.user); 
+  }
   render() {
-    const tab = createMaterialBottomTabNavigator
     return(
       // <Login />
       <View style={{flex: 1}}>
-        {/* <Text>Helo From Home</Text> */}
+        <Text>Helo From Home</Text>
         <View style={{flex: 1}}>
-
+          <Text>{this.state.user.email}</Text>
+          <Text>{this.state.user.username}</Text>
         </View>
         <View style={{backgroundColor: 'blue'}}>
           <NavbarButton />
