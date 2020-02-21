74c82f470cef4350320a3e699052ef2f1a7ce70a
akwanmaroso <akwanmaroso@gmail.com>
2020-02-21 09:02:48 +0800

    add module react-navigation and create component navbar

--- mobileApp/App.js
+++ mobileApp/App.js
@@ -6,13 +6,26 @@ import {
   View,
   Text,
   StatusBar,
+  Image
 } from 'react-native';
 import Login from './src/pages/Login'
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs'
+import NavbarButton from './src/NavbarBtn';
 
 class App extends Component {
   render() {
+    const tab = createMaterialBottomTabNavigator
     return(
-      <Login />
+      // <Login />
+      <View style={{flex: 1}}>
+        {/* <Text>Helo From Home</Text> */}
+        <View style={{flex: 1}}>
+
+        </View>
+        <View style={{backgroundColor: 'blue'}}>
+          <NavbarButton />
+        </View>
+      </View>
     )
   }
 }
