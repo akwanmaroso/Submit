3aa0bc9cc8a41614bf9a5a777b431248521f45c1
akwanmaroso <akwanmaroso@gmail.com>
2020-02-22 18:33:02 +0800

    update styling login

--- mobileApp/src/pages/Login/index.js
+++ mobileApp/src/pages/Login/index.js
@@ -1,52 +1,87 @@
 import React, { Component } from 'react';
 import {
+  SafeAreaView,
   StyleSheet,
+  ScrollView,
   View,
   Text,
+  StatusBar,
   TextInput,
+  Image,
   TouchableOpacity
 } from 'react-native';
+import Axios from 'axios';
 
 export default class Login extends Component {
+  state = {
+    username: '',
+    password: '',
+  }
+  handleLogin = async() => {
+    try {
+      const login = await Axios.post('http://10.0.2.2:4900/api/login', {
+        username: this.state.username,
+        password: this.state.password
+      });
+      console.log(login.data.data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: '#00E676', justifyContent: 'center' }}>
-        <View style={{ alignItems: 'center' }}>
-          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>SAKU-KU</Text>
+      <ScrollView style={{ flex: 1, backgroundColor: '#07AC69', paddingTop: 65}}>
+        <View style={{ flex: 1, marginBottom: 81}}>
+          <View style={{ alignItems: 'center'}}>
+            <Text style={{ fontSize: 31, color:'#ffffff' }}>Sign In</Text>
+          </View>
+          <View style={{ marginHorizontal: 7, marginTop: 39 }}>
+            <View>
+              <TextInput 
+                style={styles.input} 
+                placeholder="Username"
+                onChangeText={(username) => this.setState({username})} 
+                />
+            </View>
+            <View style={{ marginTop: 15 }}>
+              <TextInput 
+                style={styles.input} 
+                onChangeText={(password) => this.setState({password})} 
+                placeholder="Password"
+                />
+            </View>
+            <View style={{ alignSelf: 'center', marginTop: 45 }}>
+              <TouchableOpacity style={{ backgroundColor: '#ffffff', padding: 16, borderRadius: 25}} onPress={() => this.handleLogin()}>
+                <Text style={{ fontSize: 16, color: '#07AC69', textAlign: 'center', paddingHorizontal: 46 }}>Login</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-        <View style={{ marginHorizontal: 16 }}>
-          <View>
-            <Text style={styles.label}>Email</Text>
-            <TextInput style={styles.input} />
-          </View>
-          <View style={{ marginTop: 15 }}>
-            <Text style={styles.label}>Password</Text>
-            <TextInput style={styles.input} />
-          </View>
-          <View style={{ alignSelf: 'center', marginTop: 20 }}>
-            <TouchableOpacity style={{ backgroundColor: '#eb3434', padding: 16, borderRadius: 25, }}>
-              <Text style={{ fontSize: 16, color: 'white', textAlign: 'center', paddingHorizontal: 46 }}>Login</Text>
-            </TouchableOpacity>
+        <View style={{position: 'relative'}}>
+          <View style={{  height: 233, backgroundColor: '#ffffff', justifyContent: 'flex-end', paddingBottom: 80,  alignItems: 'center'}}>
+            <Text>Don't have account ?</Text>
+            <Text style={{ color: '#07AC69'}}>Sign Up</Text>
           </View>
         </View>
-      </View>
+      </ScrollView>
     )
   }
 }
 
 const styles = StyleSheet.create({
   label: {
-    fontSize: 17,
-    fontWeight: 'bold',
-    color: '#1C1C1C'
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#ffffff'
   },
   input: {
     height: 54,
     width: '100%',
-    paddingLeft: 10,
-    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    backgroundColor: '#32BB83',
     marginTop: 4,
     borderRadius: 10,
     fontSize: 15,
+    color: '#ffffff'
   }
 })
